e056e2191dfc1f902d5216283ae33230822495af
Ravil <ravilgab@yahoo.com>
2022-11-20 00:53:16 +0600

    Project created

---
+++ Homework_2.28_RandomNumberRepeat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_2.28_RandomNumberRepeat
+//
+//  Created by Ravil on 20.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
