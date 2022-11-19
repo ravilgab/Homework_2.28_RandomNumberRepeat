98b1e68a4cf42180d15b14332c59eb18f24d07f2
Ravil <ravilgab@yahoo.com>
2022-11-20 01:46:08 +0600

    Storyboard is ready.

---
+++ Homework_2.28_RandomNumberRepeat/SettingsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsViewController.swift
+//  Homework_2.28_RandomNumberRepeat
+//
+//  Created by Ravil on 20.11.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+}
