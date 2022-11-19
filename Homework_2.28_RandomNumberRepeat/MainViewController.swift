abad6e7b0bffb143cbb549385ae7197c12326b8a
Ravil <ravilgab@yahoo.com>
2022-11-20 03:22:54 +0600

    Project is ready.

--- Homework_2.28_RandomNumberRepeat/MainViewController.swift
+++ Homework_2.28_RandomNumberRepeat/MainViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var minimumLabel: UILabel!
+    @IBOutlet weak var maximumLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var generateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        generateButton.layer.cornerRadius = 12
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        
+        settingsVC.minimumValue = minimumLabel.text
+        settingsVC.maximumValue = maximumLabel.text
     }
 
-
+    @IBAction func generateButtonPressed() {
+        let minimumValue = Int(minimumLabel.text ?? "") ?? 0
+        let maximumValue = Int(maximumLabel.text ?? "") ?? 100
+        
+        resultLabel.text = String(Int.random(in: minimumValue...maximumValue))
+    }
+    
+    @IBAction func unwindSegue(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.source as? SettingsViewController else { return }
+        
+        minimumLabel.text = settingsVC.minimumTF.text
+        maximumLabel.text = settingsVC.maximumTF.text
+    }
 }
 
--- Homework_2.28_RandomNumberRepeat/SettingsViewController.swift
+++ Homework_2.28_RandomNumberRepeat/SettingsViewController.swift
@@ -9,4 +9,28 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    @IBOutlet weak var minimumTF: UITextField!
+    @IBOutlet weak var maximumTF: UITextField!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    var minimumValue: String!
+    var maximumValue: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        minimumTF.text = minimumValue
+        maximumTF.text = maximumValue
+        
+        doneButton.layer.cornerRadius = 12
+        cancelButton.layer.cornerRadius = 12
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {                       self.view.endEditing(true)
+    }
+    
+    @IBAction func cancelButtonPressed() {
+        dismiss(animated: true)
+    }
 }
